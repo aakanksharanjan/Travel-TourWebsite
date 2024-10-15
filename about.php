2f3ba643586d871c64ff3becdd324e6a82aa8c88
aakanksharanjan <aak.ran.rt23@dypatil.edu>
2024-10-16 03:30:23 +0530

    finished footer and navbar design

---
+++ about.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>About</title>
+
+       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+
+       <link rel="stylesheet" href=https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css>
+
+       <link rel="stylesheet" href="/style.css">
+</head>
+<body>
+
+ <section class="header">
+
+     <a href="home.php" class="logo">Travel.</a>
+
+     <nav class="navbar">
+        <a href=“home.php”>Home</a>
+        <a href="about.php">About</a> 
+        <a href="package.php">Package</a> 
+        <a href="book.php">Book</a> 
+     </nav>
+
+     <div id="menu-btn" class="fas fa-bars"></div>
+</section> 
+ 
+<div class="heading" style="background:url(images/heading-img-1.png) no-repeat ">
+    <h1>About us</h1>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+      <h3>Quick Links</h3>
+         <a href=“home.php”><i class="fas fa-angle-right"></i>Home</a>
+         <a href="about.php"><i class="fas fa-angle-right"></i>About</a> 
+         <a href="package.php"><i class="fas fa-angle-right"></i>Package</a> 
+         <a href="book.php"><i class="fas fa-angle-right"></i>Book</a> 
+      </div>
+       
+      <div class="box">
+         <h3>Extra Links</h3>
+         <a href="#"><i class="fas fa-angle-right"></i>Ask questions</a>
+         <a href="#"><i class="fas fa-angle-right"></i>About us</a>
+         <a href="#"><i class="fas fa-angle-right"></i>Privacy policy</a>
+         <a href="#"><i class="fas fa-angle-right"></i>Terms of use</a>
+   </div>
+
+   <div class="box">
+      <h3>Contact Info</h3>
+      <a href="#"><i class="fas fa-phone"></i> +123-456-789 </a>
+      <a href="#"><i class="fas fa-phone"></i> +111-222-333 </a>
+      <a href="#"><i class="fas fa-envelope"></i> aakanksha.ranjan@outlook.com </a>
+      <a href="#"><i class="fas fa-map"></i> Mumbai, India - 400104 </a>
+   </div>
+
+      <div class="box">
+         <h3>Follow Us</h3>
+         <a href="#"><i class="fab fa-instagram"></i>Instagram</a>
+         <a href="#"><i class="fab fa-facebook-f"></i>Facebook</a>
+         <a href="#"><i class="fab fa-linkedin"></i>Linkedin</a>
+         <a href="#"><i class="fab fa-twitter"></i>Twitter</a>
+      </div>
+   </div>
+      <div class="credit">Created by <span>RAIT AIML Students</span> | All rights reserved! | </div>
+
+      
+</section>
+
+
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
+<!-- custom js file link -->
+<script src="/script.js"></script>
+</body>
+</html>  
